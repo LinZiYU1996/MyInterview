d07d2d638ef1564baa460787dc6562e7a24acd55
LinZiYu1996 <2669093302@qq.com>
2019-06-16 23:39:00 +0800

    interview

---
+++ src/Collection/Map/ConcurrentHashMapDemo/Test.java
@@ -0,0 +1,19 @@
+package Collection.Map.ConcurrentHashMapDemo;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @Author: Mr.Lin
+ * @Description:
+ * @Date: Create in 23:20 2019/6/16
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        ConcurrentHashMap<Integer,Integer> hashMap = new ConcurrentHashMap<>();
+
+
+
+    }
+}
---
+++ src/Collection/Map/HashMapDemo/HashMapTest.java
@@ -0,0 +1,18 @@
+package Collection.Map.HashMapDemo;
+
+import java.util.HashMap;
+
+/**
+ * @Author: Mr.Lin
+ * @Description:
+ * @Date: Create in 11:57 2019/6/16
+ */
+public class HashMapTest {
+
+
+    public static void main(String[] args) {
+
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+    }
+}
