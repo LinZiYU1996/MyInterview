72135f985c97df222d720afa0775456a066e18c0
LinZiYu1996 <2669093302@qq.com>
2019-06-20 22:01:19 +0800

    interview

---
+++ src/Collection/List/ArrayListDemo/Test.java
@@ -0,0 +1,19 @@
+package Collection.List.ArrayListDemo;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: Mr.Lin
+ * @Description:
+ * @Date: Create in 22:00 2019/6/20
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+    }
+}
