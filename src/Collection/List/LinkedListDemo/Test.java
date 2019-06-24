bc8490ac7d21bf9c793bc170585bc90fd4b96f91
LinZiYu1996 <2669093302@qq.com>
2019-06-24 15:08:18 +0800

    interview

---
+++ src/Collection/List/LinkedListDemo/Test.java
@@ -0,0 +1,17 @@
+package Collection.List.LinkedListDemo;
+
+import java.util.LinkedList;
+
+/**
+ * @Author: Mr.Lin
+ * @Description:
+ * @Date: Create in 22:22 2019/6/20
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+    }
+}
